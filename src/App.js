7d24e6dce3be0cde32eeaf205933b566d629576e
SANEH69 <sanehthandoh02@gmail.com>
2025-04-15 19:26:17 +0200

    Introduced a footer class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Weather from "./Weather";
+import Footer from "./Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="outer" >
+      <div className="inner" >
       <header className="App-header">
-      <Weather city="Durban" />
+        <Weather city="Durban" />
+        <Footer />
       </header>
+    </div>
+    </div>
     </div>
   );
 }
