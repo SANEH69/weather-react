8511e13b542082142107252df8fe7e0c99b59f95
SANEH69 <sanehthandoh02@gmail.com>
2025-04-15 16:44:11 +0200

    Introduced useState in my project

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 
 function Weather(props) {
+    let [city, setCity] = useState(" ");
+    let [weather, setWeather] = useState (" ");
+
     function displayWeather(response) {
-        alert(`The temperature in ${props.city} is ${Math.round(response.data.temperature.current)}°C`) 
+        console.log(response);
+        alert(`The temperature in ${props.city} is ${Math.round(response.data.temperature.current)}°C`);
     }
 
     let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
     axios.get(apiUrl).then(displayWeather);
 
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
+    let form = (
+        <form>
+            <input type="search" onChange={updateCity}/>
+            <input type="submit" />
+        </form>
+    );
+
     return (
-       <h1>Hello</h1>
+       <h1>{form}</h1>
     );
 }
 export default Weather;
