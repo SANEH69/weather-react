ef131b3f8a47e050df7d0e7e6669d2fd79c34010
SANEH69 <sanehthandoh02@gmail.com>
2025-04-15 19:27:17 +0200

    Created a Footer page and made it importable

---
+++ src/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+    return <h5>This project was created by Sanelisiwe Ngubane and is <a href="https://github.com/SANEH69/weather-react">open-sourced</a>.</h5>
+}
