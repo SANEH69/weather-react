5241ff74e098fcfb861f2c9caab80fef3fd66813
SANEH69 <sanehthandoh02@gmail.com>
2025-04-19 16:36:30 +0200

    Fetched and displayed real time date and time

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
-import Date from "./Date";
+import WeatherDate from "./WeatherDate";
 import Units from "./Units";
 import './Weather.css'
 
@@ -52,7 +52,7 @@ function Weather(props) {
             <h1>{form}</h1>
             <div className="weatherConditions">
                 <div>
-                    <Date code={weather.date} />
+                    <WeatherDate data={weather.date} />
                     <h2>{weather.city},{weather.country}</h2>
                     <WeatherIcon code={weather.icon} />
                     <p className="description">{weather.condition}</p>
---
+++ src/WeatherDate.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function WeatherDate(props) {
+    console.log(props.data);
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let hours = props.data.getHours();
+    let minutes = props.data.getMinutes();
+    let date = props.data.getDate();
+    let day = days[props.data.getDay()];
+    let month = months[props.data.getMonth()];
+
+    if (minutes < 10) {
+        return <span>(`0${minutes}`)</span>
+    };
+    if (hours < 10) {
+        return <span>(`0${hours}`)</span>
+    };
+    return <div className="date">{month} {date}, {day} {hours} : {minutes}</div>;
+}
