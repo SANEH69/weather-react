af1e55e3693f714533f23ab5b547bb27a6fa5b48
SANEH69 <sanehthandoh02@gmail.com>
2025-04-20 19:02:57 +0200

    Updated my daily forecast and added a design

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -5,11 +5,15 @@ import "./DailyForecast.css"
 export default function DailyForecast() {
     return  (
     <div className="dailyForecast">
+        <div className="dailyTemp">
+        <p>April 20, Sunday 18 : 59</p>
+        <h1>Durban, South Africa</h1>
         <p>Monday</p>
         <p>💧 49%</p>
-        <WeatherIcon code="clear-sky-day" size={20}/>
-        <p>29°C</p>
-        <p>17°C</p>
+        <WeatherIcon code="clear-sky-day" size={30}/>
+        <span><p>29°C</p></span>
+        <span className="minTemp"><p>17°C</p></span>
+        </div>
     </div>
     );
 }
